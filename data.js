3d5a98305ceffea93debc4a6267cc096f45743c3
Carlead24 <79675296+Carlead24@users.noreply.github.com>
2022-04-10 07:04:52 +0100

    Initial

---
+++ data.js
@@ -0,0 +1,38 @@
+let songs = [
+    {
+        name: 'Leave the door open',
+        path: 'music/Bruno-Mars-Anderson-.Paak-Silk-Sonic-Leave-the-Door-Open.mp3',
+        artist:'Bruno Mars x Anderson Paak x Silk Sonic',
+        cover: 'images/Bruno.jpg'
+    },
+
+    {
+        name:'Kwaku The Traveler',
+        path:'music/Black Sherif - Kwaku The Traveler.mp3',
+        artist:'Black Sherif',
+        cover: 'images/Black.jpeg'
+    },
+
+    {
+        name: 'Undecided',
+        path:'music/Chris brown-Undecided.mp3',
+        artist:'Chris brown',
+        cover:'images/Chris.jpg'
+    },
+
+    {
+        name:'Naked',
+        path:'music/Ella Mai - Naked.mp3',
+        artist:'Ella Mai',
+        cover:'images/Ella.jpg'
+    },
+
+    {
+        name:'Davido - Intro',
+        path:'music/Davido - Intro.mp3',
+        artist:'Davido - Intro',
+        cover:'images/Davido.jpeg'
+    }
+
+]
+
